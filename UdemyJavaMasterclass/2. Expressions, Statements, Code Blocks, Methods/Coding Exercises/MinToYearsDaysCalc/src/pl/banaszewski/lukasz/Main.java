ed0097f0c3e71fabba4eb73f09532d22e5d5ea7f
Jutabe <lukasz@banaszewski.pl>
2020-04-17 16:41:02 +0200

    Adding Coding Exercise solution - Minutes to Years and Days Calculator

---
+++ UdemyJavaMasterclass/2. Expressions, Statements, Code Blocks, Methods/Coding Exercises/MinToYearsDaysCalc/src/pl/banaszewski/lukasz/Main.java
@@ -0,0 +1,11 @@
+package pl.banaszewski.lukasz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+    }
+}
