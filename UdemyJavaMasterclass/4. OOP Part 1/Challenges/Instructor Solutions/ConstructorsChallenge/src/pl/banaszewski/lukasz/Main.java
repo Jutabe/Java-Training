2103171d7b4775c5be95316245169f599b26bf48
Jutabe <lukasz@banaszewski.pl>
2020-04-29 19:09:38 +0200

    Adding instructor solution - Constructors Challenge

--- UdemyJavaMasterclass/4. OOP Part 1/Challenges/Instructor Solutions/ConstructorsChallenge/src/pl/banaszewski/lukasz/Main.java
+++ UdemyJavaMasterclass/4. OOP Part 1/Challenges/Instructor Solutions/ConstructorsChallenge/src/pl/banaszewski/lukasz/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /* Lecture and Challenge Part 1 with the use of Account.java Class.
+
         //Account bobsAccount = new Account("12345", 0.00, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
 
         Account bobsAccount = new Account();
@@ -19,6 +21,20 @@ public class Main {
         bobsAccount.deposit(51.0);
         bobsAccount.withdrawal(100.0);
 
+        Account timsAccount = new Account("Tim", "tim@email.com", "12345");
+        System.out.println(timsAccount.getNumber() + " name " + timsAccount.getCustomerName());
+
+         */
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getEmailAddress());
 
 
     }
