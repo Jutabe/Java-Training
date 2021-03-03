479f40e474e94df78a8306f55731dba6c5b75c88
Jutabe <lukasz@banaszewski.pl>
2021-03-03 11:50:19 +0100

    Saving progress

--- UdemyJavaMasterclass/7. Inner and Abstract Classes & Interfaces/Challenges/My Solutions/AbstractionChallenge/src/pl/banaszewski/lukasz/ListItem.java
+++ UdemyJavaMasterclass/7. Inner and Abstract Classes & Interfaces/Challenges/My Solutions/AbstractionChallenge/src/pl/banaszewski/lukasz/ListItem.java
@@ -1,17 +1,36 @@
 package pl.banaszewski.lukasz;
 
-public abstract class ListItem {
-    private String value;
+import java.util.List;
 
-    public ListItem(String value) {
-        this.value = value;
+public abstract class ListItem {
+    private String listName;
+    private int currentIndex = 0;
+    private List<String> list;
+
+    public ListItem(String listName, String[] stringArray) {
+        this.listName = listName;
+        for(String string : stringArray) {
+            this.list.add(string);
+        }
     }
 
     public abstract void next();
     public abstract void previous();
     public abstract void compareTo();
 
-    public String getValue() {
-        return value;
+    public String getListName() {
+        return listName;
+    }
+
+    public int getCurrentIndex() {
+        return currentIndex;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setCurrentIndex(int currentIndex) {
+        this.currentIndex = currentIndex;
     }
 }
--- UdemyJavaMasterclass/7. Inner and Abstract Classes & Interfaces/Challenges/My Solutions/AbstractionChallenge/src/pl/banaszewski/lukasz/MyLinkedList.java
+++ UdemyJavaMasterclass/7. Inner and Abstract Classes & Interfaces/Challenges/My Solutions/AbstractionChallenge/src/pl/banaszewski/lukasz/MyLinkedList.java
@@ -1,5 +1,43 @@
 package pl.banaszewski.lukasz;
 
-public class MyLinkedList  {
+public class MyLinkedList extends ListItem{
 
+    public MyLinkedList(String listName, String[] list) {
+        super(listName, list);
+    }
+
+    @Override
+    public void next() {
+        if(getList().size() < 1) {
+            System.out.println(getListName() + " is empty, please add some values first.");
+            return;
+        }
+
+        if(getList().size() <= getCurrentIndex()) {
+            System.out.println(getListName() + " have reached the end of the list.");
+            return;
+        } else {
+            setCurrentIndex(getCurrentIndex() + 1);
+        }
+    }
+
+    @Override
+    public void previous() {
+        if(getList().size() < 1) {
+            System.out.println(getListName() + ": This list is empty, please add some values first.");
+            return;
+        }
+
+        if(getCurrentIndex() < 0) {
+            System.out.println(getListName() + ": Already on the start of the list.");
+            return;
+        } else {
+            setCurrentIndex(getCurrentIndex() - 1);
+        }
+    }
+
+    @Override
+    public void compareTo() {
+
+    }
 }
